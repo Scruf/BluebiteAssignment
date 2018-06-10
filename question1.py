3e2503138aa234a533ab8424844bb0354d47c8de
Scruf <ekozitski@hotmail.com>
2018-06-10 13:41:55 -0400

    Imitial Commit

---
+++ question1.py
@@ -0,0 +1,12 @@
+def base36_to_base10(sample):
+	num = 0
+	power = 1
+	alphabet = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O',
+				'P','Q','R','S','T','U','V','W','X','Y','Z']
+	for i,s in enumerate(sample):
+		print(len(sample)-i-1)
+		num += alphabet.index(s.upper())*(36**(len(sample)-i-1))
+	print(num)
+
+if __name__ == '__main__':
+	base36_to_base10("ixqye")
